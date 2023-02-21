f83bd8d8c33d48ea7505eb38a4fe1a12ef60ccd1
Yasmim Paiva <yasmimpaiva0303@gmail.com>
2023-02-21 16:57:28 -0300

    avatar changes

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ function toggleMode() {
 
    if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar.png")
 
    } else {
     // se tiver sem light mode, deixar a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar-night.jpg")
    }
 }
